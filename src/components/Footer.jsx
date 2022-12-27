76cfbed5113e13cb6b27fa1ebb7a485bddd1e24f
McRonaah <ronaldcheruiyot342@gmail.com>
2022-12-27 17:53:47 +0300

    Update target blank on whatsapp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer () {
       <div className="footer">
           <div className="section1">
               <h4>Contact me</h4>
-                <p><a href='https://wa.me/+254702233145'><FaWhatsapp/>+254702233145</a></p>
+                <p><a href='https://wa.me/+254702233145' target="_blank" rel="noreferrer"><FaWhatsapp/>+254702233145</a></p>
                 <p> <a href="tel:+254111310907" target="_blank" rel="noreferrer"><em class="lnr lnr-phone-handset"></em><FaPhone />+254111310907 </a></p>
                 <p><FaRegEnvelope /><a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=ronaldcheruyiot342@gmail.com" target="_blank" rel="noreferrer">ronaldcheruiyot342@gmail.com</a> &reg;</p>
                 <p><FaGithub/>All rights reserved &trade; inc.</p>
