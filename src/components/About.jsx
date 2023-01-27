15be92d0d42e7f62c5f031c21f3a5b53d5189583
McRonaah <ronaldcheruiyot342@gmail.com>
2023-01-27 11:37:56 +0300

    updated images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../CSS/About.css'
 import resume from '../Docs/Ronald_Kipchirchir_CV.pdf'
-import avatar1 from '../images/avatar1.png';
+import avatar1 from '../images/avatar1.webp';
 import {  FaLinkedin, FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
 
 function About() {
