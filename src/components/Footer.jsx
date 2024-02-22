b65658191f7f6df0f328105869439de9f5861556
McRonaah <ronaldcheruiyot342@gmail.com>
2024-02-23 01:47:52 +0300

    :fire: project setup :tada:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
-import '../CSS/Footer.css'
-import { FaPhone, FaRegEnvelope,  FaGithub, FaWhatsapp, } from "react-icons/fa";
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
-
-function Footer () {
+function Footer() {
   return (
-      <div className="footer">
-          <div className="section1">
-              <h4>Contact me</h4>
-                <p><a href='https://wa.me/+254702233145' target="_blank" rel="noreferrer"><FaWhatsapp/>+254702233145</a></p>
-                <p> <a href="tel:+254111310907" target="_blank" rel="noreferrer"><em class="lnr lnr-phone-handset"></em><FaPhone />+254111310907 </a></p>
-                <p><FaRegEnvelope /><a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=ronaldcheruiyot342@gmail.com" target="_blank" rel="noreferrer">ronaldcheruiyot342@gmail.com</a> &reg;</p>
-                <p><FaGithub/>All rights reserved &trade; inc.</p>
-                <p>CopyRight By Ronald Kipchirchir&#169;</p>
-          </div>
+    <div className="footer bg-gray-300 flex flex-col md:flex-row items-center justify-between p-4">
+      <div className="left-section flex gap-4 items-center text-center mb-4 md:mb-0">
+        <FaGithub className="text-2xl" />
+        <p className="text-sm">CopyRight By Ronald Kipchirchir&copy;</p>
       </div>
-  )
+      <p className="text-sm italic">Last Updated: Feb. 24, 2024</p>
+    </div>
+  );
 }
 
 export default Footer;
